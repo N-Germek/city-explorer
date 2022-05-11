e0c91183bdccf822b2545abbaa7d62820911379f
Natalija Germek <n.germek@icloud.com>
2022-05-10 22:27:43 -0700

    debugged app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
 import React from 'react';
 import axios from 'axios';
 
@@ -7,32 +7,33 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchQuery: ""
+      searchQuery: "",
+      locationName: ""
     }
   }
-getLocation = async () => {
-  const url = `https://us1.locationiq.com/v1/seatch.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
-  const response = await axios.get(url);
-  console.log("Response from axios", response.date[0].display_name);
-  this.setState({ locationName: response.data[0].display_name});
-}
+  getLocation = async () => {
+    const url = `https://us1.locationiq.com/v1/seatch.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
+    const response = await axios.get(url);
+    console.log("Response from axios", response.date[0].display_name);
+    this.setState({ locationName: response.data[0].display_name });
+  }
 
-}
 
 render() {
-  console.log("this.state in App.js: ")
+  console.log("this.state in App.js: ", this.state);
   return (
     <div className="App">
       <h1>Welcome to City Explorer</h1>
-      <input onChange={(event) => this.setState({searchQuery: event.target.value})}
-      placeholder="sesarch for a city!"
+      <input onChange={(event) => this.setState({ searchQuery: event.target.value })}
+        placeholder="search for a city!"
       />
       <button onClick={this.getLocation}>Explore here!</button>
       {this.state.locationName &&
-      <h2>The city you searched for is: </h2>
+        <h2>The city you searched for is{this.state.locationName}</h2>
       }
     </div>
   );
+}
 }
 
 export default App;
