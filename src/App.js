e0ebcbf06bb5c21643288232cddf7e0edf6a6fde
Natalija Germek <n.germek@icloud.com>
2022-05-10 21:25:10 -0700

    initial build out of city explorer project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: ""
+    }
+  }
+getLocation = async () => {
+  const url = `https://us1.locationiq.com/v1/seatch.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
+  const response = await axios.get(url);
+  console.log("Response from axios", response.date[0].display_name);
+  this.setState({ locationName: response.data[0].display_name});
+}
+
+}
+
+render() {
+  console.log("this.state in App.js: ")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to City Explorer</h1>
+      <input onChange={(event) => this.setState({searchQuery: event.target.value})}
+      placeholder="sesarch for a city!"
+      />
+      <button onClick={this.getLocation}>Explore here!</button>
+      {this.state.locationName &&
+      <h2>The city you searched for is: </h2>
+      }
     </div>
   );
 }
