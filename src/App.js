bb6c5e8bdc72180e101b794b12e73be08d14f22e
Natalija Germek <n.germek@icloud.com>
2022-05-24 20:31:35 -0700

    properly rendered map and updated CSS

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import axios from 'axios';
 import Search from './Search';
+import Map from './Map';
 
 class App extends React.Component {
   constructor(props) {
@@ -16,7 +17,7 @@ class App extends React.Component {
     const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
     const response = await axios.get(url);
     console.log("Response from axios", response.data[0].display_name);
-    this.setState({ locationName: response.data[0]});
+    this.setState({ locationName: response.data[0] });
   }
 
   changeHandler = (e) => {
@@ -25,19 +26,22 @@ class App extends React.Component {
     })
   }
 
-render() {
-  console.log("this.state in App.js: ", this.state);
-  return (
-    <div className="App">
-      <h1>Welcome to City Explorer</h1>
-      <Search changeHandler={this.changeHandler} getLocation={this.getLocation}/>
-      
-      {this.state.locationName &&
-        <h2>The city you searched for is {this.state.locationName.display_name} Longitute{this.state.locationName.lon} Latitude {this.state.locationName.lat}</h2>
-      }
-    </div>
-  );
-}
+  render() {
+    console.log("this.state in App.js: ", this.state);
+    return (
+      <div className="App">
+        <h1>Welcome to City Explorer</h1>
+        <Search changeHandler={this.changeHandler} getLocation={this.getLocation} />
+
+        {this.state.locationName &&
+          <>
+            <h2>The city you searched for is {this.state.locationName.display_name} Longitute{this.state.locationName.lon} Latitude {this.state.locationName.lat}</h2>
+            <Map locationName={this.state.locationName}></Map>
+          </>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Error.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Error extends React.Component {
+    render() {
+        return (
+            <>
+            
+            </>
+        ) 
+    }
+}
+export default Error;
